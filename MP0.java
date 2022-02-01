8b611a896101bc34fb7905988bb5efcfda0a0548
TackoFalls <69122331+TackoFalls@users.noreply.github.com>
2022-01-31 23:53:23 -0500

    MP0 Java Assignment

---
+++ MP0.java
@@ -0,0 +1,16 @@
+
+public class MP0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String yourName = "Daniel";
+		
+		System.out.println("Hello World!");
+		System.out.println("My name is " + (yourName));
+		
+		
+		
+	}
+
+}
